a1a821b3dacfec461b565a7658b4ca0f7ebe16ef
flamez <1050355217@qq.com>
2017-09-26 17:39:32 +0800

    1.1

--- Flamez57Upload.php
+++ Flamez57Upload.php
@@ -30,6 +30,11 @@ class Flamez57Upload
 	private $israndname=true; 
 
 	/*
+	** 是否覆盖同名文件
+	*/
+	private $over_write=false;
+
+	/*
 	**	源文件名称
 	*/
 	private $originName; 
@@ -127,6 +132,12 @@ class Flamez57Upload
 			case -5: 
 				$str .= "必须指定上传文件的路径"; 
 				break;
+			case -6:
+				$str .= "同名文件已存在！";
+				break;
+			case -7:
+				$str .= "文件不存在";
+				break;
 			default: 
 				$str .= "末知错误";
 		}
@@ -143,7 +154,7 @@ class Flamez57Upload
 			return false;
 		}
 		if(!file_exists($this->filepath) || !is_writable($this->filepath)){
-			if(, 0755)){
+			if(chmod($this->filepath, 0755)){
 				$this->setOption('errorNum', -4);
 				return false;
 			}
@@ -305,6 +316,28 @@ class Flamez57Upload
 	}
 
 	/*
+	** 检查是否有同名文件，是否覆盖
+	*/
+	private function check_same_file($filename)
+	{
+		if(file_exists($filename)&&$this->over_write!=true){
+			$this->setOption('errorNum', -6);
+			return false;
+		}    
+	}
+
+	/*
+	** 检查文件是否是通过 HTTP POST 上传的
+	*/
+	private function is_upload_file($tmp_name)
+	{
+		if(!is_uploaded_file($tmp_name)){
+			$this->setOption('errorNum', -7);
+			return false;	
+		}
+	}
+
+	/*
 	** 用于获取上传后文件的文件名
 	*/
 	public function getNewFileName()
@@ -319,4 +352,76 @@ class Flamez57Upload
 	{
 		return $this->errorMess;
 	}
+ 
+/*
+** 生成缩略图
+*/
+//最大宽：120，高：120
+public function create_simg($img_w,$img_h)
+{
+$name=$this->set_name();
+$folder=$this->creat_mulu();
+$new_name="../../".$folder."/s_".$name;      
+$imgsize=getimagesize($this->files_name());
+ 
+switch ($imgsize[2]){
+case 1:
+if(!function_exists("imagecreatefromgif")){
+echo "你的GD库不能使用GIF格式的图片，请使用Jpeg或PNG格式！返回";
+exit();
+}
+$im = imagecreatefromgif($this->files_name());
+break;
+case 2:
+if(!function_exists("imagecreatefromjpeg")){
+echo "你的GD库不能使用jpeg格式的图片，请使用其它格式的图片！返回";
+exit();
+}
+$im = imagecreatefromjpeg($this->files_name());
+break;
+case 3:
+$im = imagecreatefrompng($this->files_name());
+break;
+case 4:
+$im = imagecreatefromwbmp($this->files_name());
+break;
+default:
+die("is not filetype right");
+exit;
+}
+ 
+$src_w=imagesx($im);//获得图像宽度
+$src_h=imagesy($im);//获得图像高度
+$new_wh=($img_w/$img_h);//新图像宽与高的比值
+$src_wh=($src_w/$src_h);//原图像宽与高的比值
+if($new_wh<=$src_wh){
+$f_w=$img_w;
+$f_h=$f_w*($src_h/$src_w);
+}else{
+$f_h=$img_h;
+$f_w=$f_h*($src_w/$src_h);
+}
+if($src_w>$img_w||$src_h>$img_h){      
+if(function_exists("imagecreatetruecolor")){//检查函数是否已定义
+$new_img=imagecreatetruecolor($f_w,$f_h);
+if($new_img){
+imagecopyresampled($new_img,$im,0,0,0,0,$f_w,$f_h,$src_w,$src_h);//重采样拷贝部分图像并调整大小
+}else{
+$new_img=imagecreate($f_w,$f_h);
+imagecopyresized($new_img,$im,0,0,0,0,$f_w,$f_h,$src_w,$src_h);
+}
+}else{
+$$new_img=imagecreate($f_w,$f_h);
+imagecopyresized($new_img,$im,0,0,0,0,$f_w,$f_h,$src_w,$src_h);
+}
+if(function_exists('imagejpeg')){
+imagejpeg($new_img,$new_name);
+}else{
+imagepng($new_img,$new_name);
+}
+imagedestroy($new_img);
+}
+//imagedestroy($new_img);
+return $new_name;
+} 	
 }
